da02ed43972e65b86e9c397a0134df09dbf088b4
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2016-11-23 22:14:00 +0100

    Added two result files

--- evaluations/parameters.py
+++ evaluations/parameters.py
@@ -1,6 +1,6 @@
 __author__ = 'Martin'
 class ParameterSettingRealistic():
-	def __init__(self, d=0.99, beta=2,acceptance=(0.2,0.8), nodeProbVictim=0.25, nodeProbNonVictim=0.75, nodeProbSybil=0.69, nodeProbNonSybil=0.08, edgeProbSybil=0.18, edgeProbNonSybil=0.1, sizeSmallWorld=4000, sizeSybilRegion=100, edgesSmallWorld=40, numSybils= 10, numRepeats=10, seeds=(0,1000, 1500), maxRequests=101, evalInterval=5 ):
+	def __init__(self, d=0.99, beta=2,acceptance=(0.2,0.8), nodeProbVictim=0.25, nodeProbNonVictim=0.75, nodeProbSybil=0.69, nodeProbNonSybil=0.08, edgeProbSybil=0.18, edgeProbNonSybil=0.1, sizeSmallWorld=4000, sizeSybilRegion=100, edgesSmallWorld=40, numSybils= 10, numRepeats=10, seeds=(0,1000, 1500), maxRequests=101, evalInterval=5):
 		self.d=d
 		self.beta=beta
 		self.acceptance=acceptance
