10ada2008cbaae3a59f5bb48497426ae5df1177b
Martin Schwarzmaier <martin.schwarzmaier@gmail.com>
2017-01-20 11:54:19 +0100

    ready for home

--- evaluations/evalSybilNodeProb.py
+++ evaluations/evalSybilNodeProb.py
@@ -1,50 +1,53 @@
 import pickle
 from matplotlib import pyplot as plt
 import sklearn.preprocessing as prep
+from util.calc import get_cdf, getMergedRanks, getMergedAuc
 import numpy as np
-from util.calc import get_cdf, getMergedRanks
+from util import setMatplotlib
 
 graph = 'facebook'
+sys = 'sybilframe'
 
+f, axarr = plt.subplots(2, 2, figsize=(2.8, 2.8), sharex=True, sharey=True)
+f.suptitle('Sybilframe Node Prior Influence')
 
-for sys in ('sybilframe', ):
-	f, axarr = plt.subplots(2, 2)
-	f.suptitle('Sybilframe Node Prior Influence - Peripheral Random', fontsize=14, weight='bold')
+for enu, i in enumerate((0.1, 0.3, 0.4, 0.6)):
+	perTarAll = pickle.load(open('../pickles/sybilNodeProb/sybilNodeProb{}PTar.p'.format(i, graph), 'rb'))
+	perTar = perTarAll[0]
+	paras = perTarAll[1]
+	perTarAuc = getMergedAuc(perTar)
+	print(list(perTarAuc['sybilframe'].values()))
+	ranksPerTar = getMergedRanks(perTar)
 
-	for enu, i in enumerate((0.1, 0.3, 0.5, 0.6)):
-		perTarAll = pickle.load(open('../pickles/sybilNodeProb/sybilNodeProb{}PRand.p'.format(i), 'rb'))
-		perTar = perTarAll[0]
-		paras = perTarAll[1]
+	""" plot random per"""
 
-		ranksPerTar = getMergedRanks(perTar)
+	x_h = sorted(ranksPerTar[sys][0][:-(paras.numSybils)])
+	x_s = sorted(ranksPerTar[sys][0][-(paras.numSybils):])
 
-		print(paras.numSybils)
+	scaler = prep.MinMaxScaler()
+	scaler.fit(sorted(ranksPerTar[sys][0]))
 
+	x_h = scaler.transform(x_h)
+	x_s = scaler.transform(x_s)
 
-		""" plot random per"""
+	y_h = get_cdf(x_h)
+	y_s = get_cdf(x_s)
 
-		x_h = sorted(ranksPerTar[sys][0][:-(paras.numSybils)])
-		x_s = sorted(ranksPerTar[sys][0][-(paras.numSybils):])
+	x_h = np.power(y_h,3)
+	x_s = np.power(y_s,3)
 
-		scaler = prep.MinMaxScaler()
-		scaler.fit(sorted(ranksPerTar[sys][0]))
+	ind_row = lambda enu: 0 if enu in (0,1) else 1
+	ind_col = lambda enu: 0 if enu in (0, 2) else 1
 
-		x_h = scaler.transform(x_h)
-		x_s = scaler.transform(x_s)
+	axis = axarr[ind_row(enu)][ind_col(enu)]
+	axis.set_ylim((0, 1.1))
+	#axis.set_xlim((-0.1, 1.1))
+	axis.set_ylabel('FN='+str(i))
 
+	axis.plot(x_h, y_h, 'b--', label='Honest')
+	axis.plot(x_s, y_s, 'r--', label='Sybil')
 
-		y_h = get_cdf(x_h)
-		y_s = get_cdf(x_s)
-
-
-		axarr[enu].set_ylim((0, 1.1))
-		axarr[enu].set_xlim((-0.1, 1.1))
-		axarr[enu].set_title('FN=FP='+str(i))
-
-
-
-		axarr[enu].plot(x_s, y_s, 'r--')
-		axarr[enu].plot(x_h, y_h, 'b--')
-
-
-plt.show()
+axis.legend(loc='upper left')
+plt.tight_layout()
+plt.subplots_adjust(top=0.92)
+plt.savefig('/home/martin/Dropbox/MasterGöttingen/Masterarbeit/figures/NodeProb{}.pdf'.format(graph), format='pdf')
--- evaluations/runSybilNodeProb.py
+++ evaluations/runSybilNodeProb.py
@@ -1,6 +1,5 @@
 from evaluations import parameters
 from evaluations.run_experiment import run_experiment
-
 """
 	Experiment:
 	Determine the Influence of the Sybil Node Classifier on Sybilframe
@@ -11,6 +10,7 @@ from evaluations.run_experiment import run_experiment
 	Set of variations:
 	FN = FP: 0.1, 0.2, 0.4
 
+	Yes, large node priors indicate BENIGN labels
 
 """
 
@@ -18,6 +18,6 @@ from evaluations.run_experiment import run_experiment
 for i in (0.1, 0.3, 0.4, 0.6):
 	paras = parameters.ParameterSettingsP(graph='facebook', strategy='breadthFirst', boosted=False, evalAt=(50,), numRepeats=3)
 	paras.nodeProbSybil = 1-i
-	paras.nodeProbNonSybil = i
+	paras.nodeProbNonSybil = 0.1
 	paras.numSeeds = 100
 	run_experiment(paras, saveAs='./sybilNodeProb/sybilNodeProb{}PRand.p'.format(i), systems=('sybilframe',))
